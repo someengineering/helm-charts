1e6b3c3661923b59f3b86793fd906c74a5d254f4
kushthedude <44091822+kushthedude@users.noreply.github.com>
2022-08-26 16:44:29 +0530

    feat: add ci-documentation for chart (#13)

---
+++ scripts/validate-docs.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+set -o pipefail
+
+cd charts/resoto
+
+NEW_FILE="$(helm-docs --dry-run)"
+OLD_FILE="$(cat README.md)"
+
+printf "Printing new readme..... \n\n\n\n"
+echo "$NEW_FILE"
+
+printf "\n\n\n\n"
+
+printf "Printing old readme..... \n\n\n\n\n"
+echo "$OLD_FILE"
+
+if [ "$NEW_FILE" == "$OLD_FILE" ]; then
+    echo "files have been updated or no change."
+    exit 0
+else
+    echo "file needs to be updated, please run helm-docs"
+    exit 1
+fi
